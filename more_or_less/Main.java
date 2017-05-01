6544ca7aaa14cf5b952a76d9eddbef46e7583483
Guillaume VILLEREZ <vuzi@free.fr>
2017-05-01 16:49:01 +0200

    Create Main.java

---
+++ more_or_less/Main.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void play(int max) {
+        Scanner input = new Scanner(System.in);
+        boolean playAgain = true;
+
+        while(playAgain) {
+            int number;
+            int maxGuess;
+            int minGuess;
+
+            maxGuess = max;
+            number = (int)(Math.random() * max + 1);
+            minGuess = 0;
+
+            boolean found = false;
+
+            while(!found) {
+                int guess = -1;
+                System.out.println("Enter a number between " + minGuess + " and " + maxGuess);
+                do {
+                    try {
+                        guess = input.nextInt();
+                    } catch (Exception e) {
+                        System.out.println("Please, only answer with a number");
+                        input.next();
+                    }
+                } while (guess < 0);
+
+                if(guess == number) {
+                    System.out.println("You found the number !");
+                    found = true;
+                } else if(guess < number) {
+                    System.out.println("The number is more than " + guess);
+                    minGuess = guess;
+                    found = false;
+                } else {
+                    System.out.println("The number is less than " + guess);
+                    maxGuess = guess;
+                    found = false;
+                }
+            }
+
+            System.out.println("Play again ?");
+            if("yes".equals(input.next())) {
+                playAgain = true;
+            } else {
+                playAgain = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        play(99);
+    }
+
+}
